501cdbf39380b8056dace42e5e39f970910e17c9
WarperSan <leumas.ecole@gmail.com>
2023-08-01 17:19:45 -0400

    PlaySocialUI Button

--- BTDAdventure.cs
+++ BTDAdventure.cs
@@ -1,16 +1,18 @@
 ﻿using BTD_Mod_Helper;
 using BTD_Mod_Helper.Api;
-using BTD_Mod_Helper.Api.Bloons;
-using BTD_Mod_Helper.Api.Display;
 using BTD_Mod_Helper.Extensions;
 using BTDAdventure;
-using Il2CppAssets.Scripts.Models.Bloons;
+using Il2Cpp;
+using Il2CppAssets.Scripts;
 using Il2CppAssets.Scripts.Unity;
-using Il2CppAssets.Scripts.Unity.Display;
-using Il2CppAssets.Scripts.Utils;
+using Il2CppAssets.Scripts.Unity.UI_New;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame;
 using MelonLoader;
 using System;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [assembly: MelonInfo(typeof(BTDAdventure.BTDAdventure), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -51,6 +53,83 @@ public class BTDAdventure : BloonsTD6Mod
     public override void OnApplicationStart()
     {
         ModHelper.Msg<BTDAdventure>("BTDAdventure loaded!");
+    }
+
+    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+    {
+        if (sceneName == "PlaySocialUI")
+        {
+            OnPlaySocialUI();
+        }
+    }
+
+    private bool _wasFromMe = false;
+
+    public override void OnMatchStart()
+    {
+        _wasFromMe = false; // Reset
+    }
+
+    private void OnPlaySocialUI()
+    {
+        Scene playSocialScene = SceneManager.GetSceneByName("PlaySocialUI");
+
+        if (playSocialScene.rootCount < 1)
+        {
+            ModHelper.Error<BTDAdventure>("Expected at least one root gameobject in PlaySocialUI");
+            return;
+        }
+
+        Transform rootItem = playSocialScene.GetRootGameObjects()[0].transform;
+
+        Transform contentBrowserContainer = rootItem.Find("PlaySocialScreen/BottomGroup/ContentBrowser");
+        contentBrowserContainer.GetComponent<RectTransform>().sizeDelta = new(3_726, 812);
+        // x = (padding * (# - 1) + size * #) * 1.35 = (280 * 3 + 480 * 4) * 1.35 = 3_726
+
+        Transform buttonHolder = contentBrowserContainer.Find("Buttons");
+        GameObject? buttonOG = null;
+
+        // Check for all buttons (safe check)
+        for (int i = 0; i < buttonHolder.childCount; i++)
+        {
+            Transform btnChild = buttonHolder.GetChild(i);
+
+            if (!btnChild.gameObject.activeInHierarchy)
+                continue;
+
+            if (btnChild.GetComponent<Button>() != null)
+            {
+                buttonOG = btnChild.gameObject;
+                break;
+            }
+        }
+
+        if (buttonOG == null)
+        {
+            ModHelper.Error<BTDAdventure>("No button has been found.");
+            return;
+        }
+
+        GameObject btdAdBtn = GameObject.Instantiate(buttonOG, buttonHolder);
+        btdAdBtn.name = "BTD Adventure BTN";
+        btdAdBtn.GetComponentInChildren<NK_TextMeshProUGUI>().localizeKey = "AC";
+
+        // Change Icon
+
+        btdAdBtn.GetComponent<Button>().AddOnClick(new Function(() =>
+        {
+            // Load game
+            // https://github.com/Void-n-Null/QuickGame/blob/main/QuickGame.cs W
+            InGameData.Editable.selectedMap = "MiddleOfTheRoad"; // Map type
+
+            // Does not matter
+            InGameData.Editable.selectedMode = "Clicks";
+            InGameData.Editable.selectedDifficulty = "Easy";
+            // ---
+
+            _wasFromMe = true;
+            UI.instance.LoadGame();
+        }));
     }
 }
 
